f4e82a8a0c01449012d0df02ca00b5e2e94c7bbc
Godoy <renanfgfg@gmail.com>
2022-02-02 01:10:12 -0300

    transformar e adicionar objetos no banco

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -1,18 +1,28 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
-
-		Pessoa p1 = new Pessoa(1, "Teste", "T@T.T");
-		Pessoa p2 = new Pessoa(2, "Teste2", "T2@T.T");
-		Pessoa p3 = new Pessoa(3, "Teste3", "T3@T.T");
+ 
+		Pessoa p1 = new Pessoa("Teste", "T@T.T");
+		Pessoa p2 = new Pessoa("Teste2", "T2@T.T");
+		Pessoa p3 = new Pessoa("Teste3", "T3@T.T");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 
 	}
 
--- src/main/java/dominio/Pessoa.java
+++ src/main/java/dominio/Pessoa.java
@@ -2,11 +2,23 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	
+	@Column(name="nomeCompleto")
 	private String nome;
+	
 	private String email;
 	
 	public Pessoa() {
@@ -20,6 +32,13 @@ public class Pessoa implements Serializable {
 		this.email = email;
 	}
 
+	public Pessoa(String nome, String email) {
+		super();
+
+		this.nome = nome;
+		this.email = email;
+	}
+	
 	public int getId() {
 		return id;
 	}
