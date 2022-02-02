67fb974b13c78ee83308b6f96d8b3896bfcf4937
Godoy <renanfgfg@gmail.com>
2022-02-02 01:12:44 -0300

    recebendo dados do banco e convertendo em objeto

--- src/main/java/aplicacao/Programa.java
+++ src/main/java/aplicacao/Programa.java
@@ -17,12 +17,12 @@ public class Programa {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
 		
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
-		System.out.println("Pronto");
+		Pessoa p = em.find(Pessoa.class, 1);
+		
+		System.out.println(p);
+		
+		em.close();
+		emf.close();
 
 	}
 
